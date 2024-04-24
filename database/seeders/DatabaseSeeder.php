5a2b3c0ce160cfd5241a409f141e8b5e4470e527
jlarm <117611704+jlarm@users.noreply.github.com>
2024-04-23 20:25:10 -0500

    Added db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'email@email.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
         ]);
+
+         User::factory(10)->create();
     }
 }
